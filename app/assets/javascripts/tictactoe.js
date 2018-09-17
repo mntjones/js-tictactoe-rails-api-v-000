4d8b3b8012b15008becf25a2fb6323717739aeed
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-17 20:24:50 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,17 @@
 // Code your JavaScript / jQuery solution here
-let turn = 0;
+var turn = 0;
 
 function player() {
+  if (turn % 2 === 0) {
+    return "X";
+  }
 
+  else {
+    return "O";
+  }
+}
+
+function updateState(square) {
+  // need to update game board with current player's game token using jQuery
+  $(square).text(player());
 }
